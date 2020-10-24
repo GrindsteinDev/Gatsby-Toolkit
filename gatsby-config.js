69206b6978988d1f847fc629965d3e341d308157
GrindsteinDev <73400427+GrindsteinDev@users.noreply.github.com>
2020-10-24 13:36:41 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yellowcake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'NomadArwut',
+    siteUrl: 'https://pedantic-lalande-4163c9.netlify.app/'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
