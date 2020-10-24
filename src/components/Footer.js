1511b4195508f591f3aa9d1a2a35f4f9287be581
GrindsteinDev <73400427+GrindsteinDev@users.noreply.github.com>
2020-10-24 13:29:52 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://www.instagram.com/kandylandbulldogs/">@kandylandbulldogs</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <a href="https://www.instagram.com/kandylandbulldogs/">KandyLand Bulldogs</a>.
         </span>
       </div>
     </footer>
